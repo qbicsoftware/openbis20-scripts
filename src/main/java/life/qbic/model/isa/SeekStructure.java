c41c6aa529f36351cda04a2471ee6b41f451c3f7
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2024-10-07 11:07:51 +0200

    structure

--- src/main/java/life/qbic/model/isa/SeekStructure.java
+++ src/main/java/life/qbic/model/isa/SeekStructure.java
@@ -3,18 +3,22 @@ package life.qbic.model;
 import ch.ethz.sis.openbis.generic.dssapi.v3.dto.datasetfile.DataSetFile;
 import java.util.List;
 import java.util.Map;
+import life.qbic.model.isa.GenericSeekAsset;
 import life.qbic.model.isa.ISAAssay;
-import life.qbic.model.isa.ISADataFile;
 import life.qbic.model.isa.ISASample;
 
 public class SeekStructure {
 
   private ISAAssay assay;
   private List<ISASample> samples;
-  private List<ISADataFile> dataFiles;
-  private Map<ISADataFile, DataSetFile> isaToOpenBISFile;
-  private Map<ISADataFile, String> isaFileToAssetType;
-  //ISASample isaSample = new ISASample(map, sampleTypeID, Arrays.asList(projectID));
+  private Map<GenericSeekAsset, DataSetFile> isaToOpenBISFile;
+
+  public SeekStructure(ISAAssay assay, List<ISASample> samples,
+      Map<GenericSeekAsset, DataSetFile> isaToOpenBISFile) {
+    this.assay = assay;
+    this.samples = samples;
+    this.isaToOpenBISFile = isaToOpenBISFile;
+  }
 
   public ISAAssay getAssay() {
     return assay;
@@ -24,11 +28,8 @@ public class SeekStructure {
     return samples;
   }
 
-  public Map<ISADataFile, DataSetFile> getIsaToOpenBISFiles() {
+  public Map<GenericSeekAsset, DataSetFile> getISAFileToDatasetFiles() {
     return isaToOpenBISFile;
   }
 
-  public String getAssetType(ISADataFile dataFile) {
-    return isaFileToAssetType.get(dataFile);
-  }
 }
