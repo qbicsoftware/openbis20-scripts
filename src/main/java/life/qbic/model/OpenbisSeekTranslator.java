d258d070c16aa75a36c1141640688323959a7100
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2024-10-30 21:08:06 +0100

    upate readme, rename property files, provide examples

--- src/main/java/life/qbic/model/OpenbisSeekTranslator.java
+++ src/main/java/life/qbic/model/OpenbisSeekTranslator.java
@@ -138,9 +138,9 @@ public class OpenbisSeekTranslator {
    */
   private void parseConfigs() throws IOException, ParserConfigurationException, SAXException {
     final String dataTypeToAttributeType = "openbis_datatype_to_seek_attributetype.xml";
-    final String datasetToAssaytype = "dataset_type_to_assaytype.properties";
+    final String datasetToAssaytype = "dataset_type_to_asset_type.properties";
     final String experimentTypeToAssayClass = "experiment_type_to_assay_class.properties";
-    final String experimentTypeToAssayType = "openbis_experiment_type_to_seek_assaytype.properties";
+    final String experimentTypeToAssayType = "experiment_type_to_seek_assay_type.properties";
     this.experimentTypeToAssayType = PropertyReader.getProperties(experimentTypeToAssayType);
     this.datasetTypeToAssetType = PropertyReader.getProperties(datasetToAssaytype);
     this.experimentTypeToAssayClass = PropertyReader.getProperties(experimentTypeToAssayClass);
